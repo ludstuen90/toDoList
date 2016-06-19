38a4449393c822fef84cc41ae617a0a44a6e600a
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-06-19 18:26:34 -0500

    styled and edited code

--- public/scripts/clientApp.js
+++ public/scripts/clientApp.js
@@ -1,6 +1,3 @@
-var todo= [];
-var completed= [];
-
 $(document).ready(function(){
   console.log("the bling works!");
 
@@ -26,8 +23,17 @@ $(document).on('click', '#deleteItem', function(){
             });
 
 
+            $('#taskIn').keypress(function(e){
+              var inItem = $('#taskIn').val();
+
+              if (e.which == 13) {
+                validateText(inItem);
+              }
+            });
+
 $(document).on('click', '#completeItem', function(){
         var id = $(this).data("id");
+
         var objToSend = {
           "id": id
         };
@@ -42,6 +48,7 @@ $(document).on('click', '#completeItem', function(){
         receiveList();
 
       });
+
 
 
 $(document).on('click', '#restoreItem', function(){
@@ -79,64 +86,68 @@ var receiveList = function(){
 
         var writeList = function(items){
           console.log("In writeList");
+          $('#list').hide();
           $('#list').empty();
+          $('#list').append('<h4>Items To Do:</h4>');
           for (var i = 0; i < items.length; i++){
             $('#list').append('<p>'+ items[i].todo + '</p>');
             $('#list').append('<button data-id="' + items[i].id +'" id="completeItem" name="completeItem">Complete '+ items[i].todo +'</button>');
             $('#list').append('<button data-id="' + items[i].id +'" id="deleteItem" name="deleteItem">Delete '+ items[i].todo +'</button>');
           }
+          $('#list').fadeIn();
         };
         var writeComplete = function(itemsIn){
           console.log("In restoreList");
+          $('#completed').hide();
           $('#completed').empty();
+          $('#completed').append('<h4>Completed Items</h4><br>');
           for (var j = 0; j < itemsIn.length; j++){
             $('#completed').append('<p>'+ itemsIn[j].todo + '</p>');
             $('#completed').append('<button data-id="' + itemsIn[j].id +'" id="restoreItem" name="completeItem">Oops, mark '+ itemsIn[j].todo +' as incomplete.</button>');
             $('#completed').append('<button data-id="' + itemsIn[j].id +'" id="deleteItem" name="deleteItem">Delete '+ itemsIn[j].todo +'</button>');
           }
+          $('#completed').fadeIn();
+
         };
       };
       receiveList();
 
-      $(document).on('click', '#subTask', function(){
+
+var validateText= function(validateThis){
+    var inItem = validateThis;
+        var sendString=function(string){
+          var objToSend = {
+            "todo": inItem
+          };
+          $.ajax({
+            type: 'POST',
+            url: '/sendItem',
+            data: objToSend
+          });
+      receiveList();
+        };
+        validateThis = validateThis.toUpperCase();
+        for (var k=0; k<validateThis.length; k++){
+          if ('ABCDEFGHIJLKMNOPQRSTUVWXYZ '.indexOf(validateThis.charAt(k)) !==-1){
+            console.log("we are looking at " + validateThis.charAt(k));
+            console.log("This string is acceptable");
+        }
+            else {
+            console.log("we are looking at " + validateThis.charAt(k));
+            console.log("This string is NOT acceptable");
+            alert("Sorry, you cannot add any special characters in this field.");
+            $('#taskIn').val('');
+            return false;
+        }
+      }
+      $('#taskIn').val('');
+      sendString(inItem);
+      receiveList();
+    };
+
+$(document).on('click', '#subTask', function(){
         var inItem = $('#taskIn').val();
         console.log('button clicked! And now we have ' + inItem );
-
-                  var validateText= function(validateThis){
-                    var sendString=function(string){
-                      var objToSend = {
-                        "todo": inItem
-                      };
-                      $.ajax({
-                        type: 'POST',
-                        url: '/sendItem',
-                        data: objToSend
-                      });
-                  receiveList();
-                    };
-                    validateThis = validateThis.toUpperCase();
-                    for (var k=0; k<validateThis.length; k++){
-                      if ('ABCDEFGHIJLKMNOPQRSTUVWXYZ '.indexOf(validateThis.charAt(k)) !==-1){
-                        console.log("we are looking at " + validateThis.charAt(k));
-                        console.log("This string is acceptable");
-                    }
-                        else {
-                        console.log("we are looking at " + validateThis.charAt(k));
-                        console.log("This string is NOT acceptable");
-                        alert("Sorry, you cannot add any special characters in this field.");
-                        $('#taskIn').val('');
-                        return false;
-                    }
-
-                  }
-
-                  sendString(inItem);
-
-                };
                 validateText(inItem);
-                receiveList();
-
               });
-
-
 });
